8bb2b335b562aa317879d17bdbd071660e86e959
nissy2005 <144938027+nissy2005@users.noreply.github.com>
2023-11-16 12:41:48 +0400

    Practise5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practise5.py
@@ -0,0 +1,8 @@
+#Write four strings in different variable and print the output as one string .(String concatenation.)
+
+a="Nissy"
+b="Joji"
+c="Creative computing"
+d="bathsparak"
+
+print(a,b,c,d)
