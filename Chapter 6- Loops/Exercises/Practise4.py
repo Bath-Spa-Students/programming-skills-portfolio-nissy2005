84c8155b6c71353ebc2a0f61003339fbf50cee88
nissy2005 <144938027+nissy2005@users.noreply.github.com>
2023-11-17 19:20:35 +0400

    Practise4.py

---
+++ Chapter 6- Loops/Exercises/Practise4.py
@@ -0,0 +1,8 @@
+'''Write a Python program that uses the break statement to exit a for loop when a specific condition is met.'''
+
+names = ["Neha", "Abhirami", "Helen", "Saniya", "Punya"]
+
+for name in names:
+  print(name)
+  if name == "Saniya":
+    break
