5a240bf6789c04ced2926427408590e094e9f93a
nissy2005 <144938027+nissy2005@users.noreply.github.com>
2023-11-17 15:53:49 +0400

    Practise4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practise4.py
@@ -0,0 +1,7 @@
+'''Write a python program that stores an integer and string value to variables x and y. Print the type of
+variable x and y.'''
+
+x = 18
+y = "Nissy"
+print(type(x))
+print(type(y))
